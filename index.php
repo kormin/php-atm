6a5617f28811a2a7c049f4dd41ee98ccdc326a3d
kormin <abaotom14@gmail.com>
2016-09-01 23:34:01 +0800

    add file

---
+++ index.php
@@ -0,0 +1,36 @@
+<?php
+
+define('TITLE', 'Homepage');
+require_once('common.php');
+ifsesh();
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+	<title><?=TITLE;?></title>
+
+	<link rel="author" href="https://github.com/kormin">
+
+	<link rel="stylesheet" type="text/css" href="styles.css">
+	<!-- <link href="assets/css/bootstrap.min.css" rel="stylesheet" type="text/css"> -->
+
+	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+	<!--[if lt IE 9]>
+		<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+	<![endif]-->
+</head>
+<body>
+	<header id="head">
+		<h1><?=TITLE;?></h1>
+	</header>
+	<section id="cont" style="text-align: center;">
+	</section>
+</body>
+</html>
