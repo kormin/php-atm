ce37bb3c14a9ac9d76c2317d85c12a0ad71a821b
kormin <abaotom14@gmail.com>
2016-09-13 23:38:07 +0800

    add cont

--- common.php
+++ common.php
@@ -4,4 +4,8 @@ function ifsesh() { // if session is present, redirects to home
 	if (session_status() == PHP_SESSION_NONE) {
 		session_start();
 	}
+
+	if (!empty($_SESSION)) {
+		header('Location: home.php');
+	}
 }
