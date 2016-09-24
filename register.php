951e9409e27dcb6dc34d68695490020ca9b22907
kormin <abaotom14@gmail.com>
2016-09-24 23:56:05 +0800

    add cmts

--- register.php
+++ register.php
@@ -1,4 +1,12 @@
 <?php
+/**
+* Author github: https://github.com/kormin
+* Project: ATM
+* Date Created: August 2016
+* File: Register
+* Description: Registration form
+* Additional Comments: 
+*/
 
 define('TITLE', 'Add Account');
 
